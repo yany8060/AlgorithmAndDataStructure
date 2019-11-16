b8532ff362fa1142db3ac0f55e0ebfcd7c4f1bcd
yany8060 <806055720@qq.com>
2019-11-16 18:05:38 +0800

    sort

--- src/main/java/com/yany/algorithms/sort/InsertSort.java
+++ src/main/java/com/yany/algorithms/sort/InsertSort.java
@@ -1,13 +1,48 @@
 package com.yany.algorithms.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by yanyong on 2017/2/4.
  */
 public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 8, 3, 1, 0, 6, 9, 0, 5, 4};
 
-    public static void insertSort(int[] numbers) {
-
-
+        insertSort(nums, nums.length);
+        System.out.println(Arrays.toString(nums));
     }
+
+
+    // 插入排序，a表示数组，n表示数组大小
+    public static void insertSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for(int i = 1; i < n; i++) {
+            int value = a[i];
+            int j = i - 1;
+
+            // 查找要插入的位置并移动数据
+            for(;j >= 0; j--) {
+                if(a[j] > value) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+
+            /**
+             * 另一种查找插入位置
+             while(j >=0 && a[j] > value) {
+             a[j + 1] = a[j];
+             j--;
+             }
+             */
+
+            // 由于执行了j-- 所以要+1
+            a[j + 1] = value;
+        }
+    }
+
 
 }
--- src/main/java/com/yany/algorithms/sort/SelectSort.java
+++ src/main/java/com/yany/algorithms/sort/SelectSort.java
@@ -1,8 +1,37 @@
 package com.yany.algorithms.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by yanyong on 2017/2/4.
  */
 public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 8, 3, 1, 0, 6, 9, 0, 5, 4};
 
+        selectionSort(nums, nums.length);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void selectionSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        // 总共要经过 N-1 轮比较
+        for(int i = 0; i < n; i++) {
+            int minIndex = i;
+            for(int j = i + i; j < n;j++) {
+                if(a[j] < a[minIndex]) {
+                    // 记录目前能找到的最小值元素的下标
+                    minIndex = j;
+                }
+            }
+
+            // 将找到的最小值和i位置所在的值进行交换
+            if(minIndex != i) {
+                int tmp = a[i];
+                a[i] = a[minIndex];
+                a[minIndex] = tmp;
+            }
+        }
+    }
 }
